d59a08f40d5739f86ff4121e3ced4fa31563cfe2
Steve Creswick <screswick@gmail.com>
2017-12-06 15:43:13 -0500

    Add dependencies to base app

--- tools/build.js
+++ tools/build.js
@@ -3,29 +3,31 @@ var webpack = require( 'webpack' );
 var webpackConfig = require( '../webpack.config.prod' );
 var colors = require( 'colors' );
 
-process.env.NODE_ENV = 'production';
+module.exports = function buildApp() {
+  process.env.NODE_ENV = 'production';
 
-console.log('Generating minified bundle for production via Webpack...'.blue);
+  console.log('Generating minified bundle for production via Webpack...'.blue);
 
-webpack(webpackConfig).run((err, stats) => {
-  if (err) { // so a fatal error occurred. Stop here.
-    console.log(err.bold.red);
-    return 1;
-  }
+  webpack(webpackConfig).run((err, stats) => {
+    if (err) { // so a fatal error occurred. Stop here.
+      console.log(err.bold.red);
+      return 1;
+    }
 
-  var jsonStats = stats.toJson();
+    var jsonStats = stats.toJson();
 
-  if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(error.red));
-  }
+    if (jsonStats.hasErrors) {
+      return jsonStats.errors.map(error => console.log(error.red));
+    }
 
-  if (jsonStats.hasWarnings) {
-    console.log('Webpack generated the following warnings: '.bold.yellow);
-    jsonStats.warnings.map(warning => console.log(warning.yellow));
-  }
+    if (jsonStats.hasWarnings) {
+      console.log('Webpack generated the following warnings: '.bold.yellow);
+      jsonStats.warnings.map(warning => console.log(warning.yellow));
+    }
 
-  console.log(`Webpack stats: ${stats}`);
-  console.log('Your app has been compiled in production mode and written to /public.'.green);
+    console.log(`Webpack stats: ${stats}`);
+    console.log('Your app has been compiled in production mode and written to /public.'.green);
 
-  return 0;
-});
+    return 0;
+  });
+};
