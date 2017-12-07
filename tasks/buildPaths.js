a35cb553593dc01f2b83d07de47b0472f0e04a7b
Steve Creswick <screswick@gmail.com>
2017-12-07 00:36:13 -0500

    Make build paths a function

--- tasks/buildPaths.js
+++ tasks/buildPaths.js
@@ -1,8 +1,10 @@
 const path = require('path');
 const appRoot = process.cwd();
 
-module.exports = {
-  'DIST': path.resolve( appRoot, 'dist' ),
-  'SRC': path.resolve( appRoot, 'src' ),
-  'JS': path.resolve( appRoot, 'src/js' ),
+module.exports = function() {
+  return {
+    'DIST': path.resolve( appRoot, 'dist' ),
+    'SRC': path.resolve( appRoot, 'src' ),
+    'JS': path.resolve( appRoot, 'src/js' )
+  }
 }
