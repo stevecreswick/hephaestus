1fe4acc1513126f970edef16b97b43cbe6fd7ce5
Steve Creswick <screswick@gmail.com>
2017-12-07 00:15:08 -0500

    Add loaders

--- hephaestus.js
+++ hephaestus.js
@@ -2,10 +2,23 @@ const buildEnv = require( './tasks/buildEnvironment' );
 const buildJS = require( './tasks/buildJS' );
 const buildHtml = require( './tasks/buildHtml' );
 const clean = require( './tasks/clean' );
+const processLogger = require( './utils/processLogger' );
 
 const publicDirectory = 'public';
 
 const hephaestus = {
+  loaders: function() {
+    return require( './webpack.config.loaders' )
+  },
+
+  test: function() {
+    const _this = this;
+    var loaders = this.loaders();
+    console.log(this.loaders());
+    processLogger( 'Testing Loaders Function: ', 'info' );
+    processLogger( loaders, 'data' );
+  },
+
   build: function( environment ) {
     buildEnv( environment );
     clean( publicDirectory );
@@ -13,5 +26,7 @@ const hephaestus = {
     buildHtml();
   }
 };
+
+hephaestus.test();
 
 module.exports = hephaestus;
